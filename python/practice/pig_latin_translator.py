802c8f9a146905f891ddfbd616fb7202f1f95c24
Jared <jared.wrathall@gmail.com>
2014-08-31 20:28:48 -0600

    Added subfolders

---
+++ python/practice/pig_latin_translator.py
@@ -0,0 +1,23 @@
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+# turn the original word into lowercase
+word = original.lower()
+
+if len(original) > 1 and original.isalpha():
+    
+    # get the first letter of the word
+    first = word[0]
+    
+    # put it all together
+    new_word = word[1:len(new_word)] + first + pyg
+    
+    print new_word  
+    
+elif len(original) = 0
+    
+    print original + pyg 
+    
+else:
+    print 'empty'
